96e8f0509de4f71bdbbdd8bf5d211eee2914ecfe
DimitriKem <dimitrikemaikin@gmail.com>
2019-08-03 14:00:47 +0300

    Configuration1

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import App from './App.vue';
+import router from './routes';
+
+new Vue({
+	el: '#app',
+	router,
+	render: h => h(App)
+});
+
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
